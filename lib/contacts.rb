89f82922a003b7861217caac456dbb4371f975fe
Hendrick Salomon <hsalomon86@gmail.com>
2020-01-25 17:21:20 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,7 +18,7 @@ def remove_strawberry(contacts)
   contacts.each do |name, value|
     if name == "Freddy Mercury"
       value.each do |attribue, data|
-        if data == :favorite_ice_cream_flavors
+        if attribue == :favorite_ice_cream_flavors
           data.delete_if {|ice_cream| ice_cream == "strawberry"}
         end
       end
